ca05019c55fabdda45f98246410dbbc68d74a7a0
Veljko <Veljko@DESKTOP-BIOBGS9>
2019-01-21 20:38:54 +0100

    Turned button, table and search into lightweight stateless components.

--- src/App.js
+++ src/App.js
@@ -65,63 +65,50 @@ class App extends Component {
   }
 }
 
-const Search = ({ value, onChange, children}) => {
-
-  // do something
-
+const Search = ({ value, onChange, children }) => {
   return (
   <form>
-      {children} <input 
-      type="text" 
-      value={value} 
-      onChange={onChange} />
-    </form>
+    {children} <input
+    type="text"
+    value={value}
+    onChange={onChange}
+    />
+  </form>
   );
 }
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        ))}
-      </div>
-    );
-  }
+const Table = ({ list, pattern, onDismiss }) => {
+  return (
+    <div>
+      {list.filter(isSearched(pattern)).map(item => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <Button onClick={() => onDismiss(item.objectID)}>
+              Dismiss
+            </Button>
+          </span>
+        </div>
+      ))}
+    </div>
+  )    
 }
 
-class Button extends Component {
-  render() {
-    const {
-      onClick,
-      className = '',
-      children,
-    } = this.props;
-
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    );
-  }
+const Button = ({onClick, className = '', children}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={className}
+      type="button"
+    >
+      {children}
+    </button>
+  );
 }
 
 export default App;
